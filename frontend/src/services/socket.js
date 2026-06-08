fd010b18b344d69fa10572c24b4454a757ddc4cd
Maikel Flores Navarro <159388957+MaikelFn@users.noreply.github.com>
2026-06-08 03:03:16 -0600

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const socketUrl = isLocalhost ? 'http://localhost:3000' : 'https://colonias-galacticas.onrender.com';
+
+const socket = io(socketUrl);
 
 export default socket;
